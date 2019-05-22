fe58c576ac177de5ff26d28d4daf4c86fc28d589
Eduardo Franco <egg@eduardos-air.lan>
2019-05-22 13:06:26 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("Users flipped " + cardOne);
+console.log("User flipped " + cardFour);
