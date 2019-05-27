0d97712a541782c6030136f10c60f32bf993c904
Eduardo Franco <egg@eduardos-air.lan>
2019-05-27 12:10:51 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("Users flipped " + cardsInPlay);
-console.log("Users flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Not a match! Try again")
+function checkForMatch() {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+        } else {
+            console.log("Not a match! Try again")
+            }
     }
+
+function flipCard (cardId) { 
+    checkForMatch();
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push[cards[cardId]];
+    
 }
+
+flipCard(0);
+flipCard(2);
