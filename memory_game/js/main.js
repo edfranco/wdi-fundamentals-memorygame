d48d55ede8b6113fdfca912f74067ef1d7119dd6
Eduardo Franco <egg@eduardos-air.lan>
2019-05-27 12:41:14 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,46 @@
 // array to store the cards
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank:"queen",
+        suit:"hearts" ,
+        cardImage: "images/queen-of-hearts.png"
+    },
+
+    {
+        rank:"queen",
+        suit:"diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+
+    {
+        rank:"king",
+        suit:"hearts",
+        cardImage:"images/king-of-hearts.png"
+
+    },
+
+    {
+        rank:"king",
+        suit:"diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
         if (cardsInPlay[0] === cardsInPlay[1]) {
-            console.log("You found a match!");
+            alert("You found a match!");
         } else {
-            console.log("Not a match! Try again")
+            alert("Not a match! Try again")
             }
     }
 
 function flipCard (cardId) { 
     checkForMatch();
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push[cards[cardId]];
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push[cards[cardId].rank];
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     
 }
 
